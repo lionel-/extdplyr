1365ff074d84a77f06ac232e5edd502765119d6a
Yuchen Wang <ycwang0712@gmail.com>
2017-02-23 07:31:17 +0000

    version 0.1.3

---
+++ R/utilities.R
@@ -0,0 +1,27 @@
+named_expr <- function(name, expr) {
+  stats::setNames(list(expr), nm = name)
+}
+
+last_group <- function(x) {
+  groups <- dplyr::groups(x)
+  groups[[length(groups)]]
+}
+
+check_se_column <- function(col_name) {
+  stopifnot(as.character(col_name), length(col) == 1)
+}
+
+
+
+#' Common parameters across routines
+#'
+#' @param data A \code{\link[base]{data.frame}} or \code{\link[dplyr]{tbl}}.
+#' @param col Name of the generated column. Use a bare name when using NSE
+#' functions and a character (quoted) name when using SE functions (functions
+#' that end with underscores).
+#' @param .dots Used in conjunction with \code{...} to support both explicit
+#' and implicit arguments.
+#' @keywords internal
+common_params <- function(data, col, .dots) {
+  NULL
+}
